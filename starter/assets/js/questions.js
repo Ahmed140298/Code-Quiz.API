5e9d0f30e241ace4a5e0d3f8500d2caa814becd9
Ahmed <amahamed666@gmail.com>
2023-01-13 18:55:52 +0000

    Add questions and answers to questions.js file

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,55 @@
+var quizQuestions = [
+  {
+    question: "What is the use of isNaN function?",
+    answers: ['true', 'false', 'nothing'],
+    correctAnswer: "true"
+  },
+  {
+    question: "Which symbol is used for comments in Javascript?",
+    answers: ['//', '/**/', '*//*'],
+    correctAnswer: "/**/"
+  },
+  {
+    question: "What is === operator?",
+    answers: ['equality', 'strict equality', 'nothing'],
+    correctAnswer: "strict equality"
+  },
+  {
+    question: "How can the style/class of an element be changed?",
+    answers: ['document.getElementById("myText"). style. fontSize = "20"; ', 
+    'document. getElementById ("myText"). className = "anyclass"; '],
+    correctAnswer: 'document.getElementById("myText"). style. fontSize = "20"; '
+  },
+  {
+    question: "What are all the types of Pop up boxes available in JavaScript?",
+    answers: ['alert prompt and confirm', 'confirm print and prompt', 'log alert and pop'],
+    correctAnswer: "alert prompt and confirm"
+  },
+  {
+    question: "What a pop()method in JavaScript is?",
+    answers: [' deletes the first element of an array and returns the array', 'deletes the last element of an array and return the array', 
+    'adds the first element of an array and returns the array', 'adds the last element of an array and not returns'],
+    correctAnswer: "deletes the last element of an array and return the array"
+  },
+  {
+    question: "Wht does DOM stands for?",
+    answers: ['document of models', 'document object model', 'document model'],
+    correctAnswer: "Document Object Model"
+  },
+  {
+    question: "What is the DOM?",
+    answers: ['html', 'javascfipt', 'api'],
+    correctAnswer: "api"
+  },
+  {
+    question: "when was javascript developed?",
+    answers: [1995, 1998, 1996],
+    correctAnswer: 1995
+  },
+  {
+    question: "What are JavaScript Data Types?",
+    answers: ['object function and window', 'number string boolean object and undefined', 
+    'arrays objects integer and document'],
+    correctAnswer: "number string boolean object and undefined"
+  }
+];
